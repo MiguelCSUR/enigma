49246279ff73ad945a93d3c31db675b49c60d495
MiguelCSUR <93922638+MiguelCSUR@users.noreply.github.com>
2021-12-08 20:05:16 +0100

    solved medusa

--- pruebaCrifrado.java
+++ pruebaCrifrado.java
@@ -1,9 +1,24 @@
 public class pruebaCrifrado {
     public static void main(String[] args) {
-        int codigo = (int)'a';
-        int valor = 10000000;
+        String textoCifrado = "asda completar sbdkjjl;adj sbjajhsk bjfskd flsd;";
+        String[] textoSeparado = textoCifrado.split("\\s+");
 
-        int desplazamiento = codigo - valor % 95;
-        System.out.println(desplazamiento);
+        if (textoSeparado.length >= 4) {
+            for (int i = 0; i < textoSeparado.length; i++) {
+                if (textoSeparado[i].length() < 15) {
+
+                    for (int j = 0; j < ListadoPalabras.listaPalabras().length; j++) {
+                        if (textoSeparado[i].equals(ListadoPalabras.listaPalabras()[j])) {
+                            // System.out.printf("%d %d %d %d\n", d, c, b, a);
+                            System.out.println("encontrado");
+                            for (String string : textoSeparado) {
+                                System.out.print(" " + string + " ");
+                            }
+                            System.out.println();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
